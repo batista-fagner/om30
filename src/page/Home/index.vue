e7b8471857c991b099ed68e3c8fea7650e9bcd5d
fagner.batista <fagner.batista@cwi.com.br>
2023-05-03 13:24:28 -0300

    home and sigin pages

---
+++ src/page/Home/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div class="menu_header">
+      <div class="menu_header-wellcome">
+        <p>Usuário: {{ nameUser }}</p>
+      </div>
+      <div class="menu_header-close">
+        <span @click="logoff">Sair</span>
+      </div>
+    </div>
+    <div class="menu-side">
+      <div class="profile_image">
+        <img src="../../assets/woman_profile.jpeg" width="100px" />
+      </div>
+    </div>
+    <el-row>
+      <el-col :span="19">
+        <Patient />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Patient from "../../components/Patient.vue";
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+  data() {
+    return {
+      nameUser: "",
+    };
+  },
+  components: {
+    Patient,
+  },
+  mounted() {
+    const name = localStorage.getItem("nameUser");
+    // Primeira letra do nome maiúscula
+    this.nameUser =  name[0].toUpperCase() + name.substring(1)
+  },
+  methods: {
+    logoff() {
+      this.$router.push({ name: "Sigin" });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.menu_header {
+  background: linear-gradient(118.62deg, #003368 19.38%, #0069c5 129.43%);
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 150px;
+}
+.menu_header-wellcome {
+  padding-left: 300px;
+  align-self: center;
+  color: #fff;
+  font-size: 1.5rem;
+}
+.menu_header-close {
+  cursor: pointer;
+  color: #fff;
+  font-weight: 700;
+}
+.profile_image {
+  padding-left: 20px;
+}
+.profile_image img {
+  border-radius: 50%;
+}
+.menu-side {
+  background: linear-gradient(118.62deg, #003368 19.38%, #0069c5 129.43%);
+  width: 150px;
+  height: 100%;
+  position: fixed;
+}
+.el-row {
+  display: flex;
+  justify-content: center;
+  margin: 80px;
+}
+</style>
---
+++ src/page/Sigin/index.vue
@@ -0,0 +1,130 @@
+<template>
+  <el-row>
+    <el-col :span="12" class="signin_left">
+      <div class="signin_sus">
+        <img src="../../assets/sus.svg" width="400px" />
+        <p class="title_login">Sistema de cadastro de saúde</p>
+      </div>
+      <div class="signin_logo-gov">
+        <img src="../../assets/logo-govbr.svg" width="100px" />
+      </div>
+    </el-col>
+    <el-col :span="12" class="signin_right">
+      <div>
+        <span label-position="top" class="sigin_text_input"
+          >Digite o seu nome</span
+        >
+        <el-input
+          placeholder="Please input"
+          class="input-right"
+          v-model="name"
+          @input="nameInput"
+        ></el-input>
+        <el-button type="primary" :disabled="isActiveButton" class="sigin_button" @click="onSignin"
+          >Entrar</el-button
+        >
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Sigin",
+  data() {
+    return {
+      name: "",
+      isActiveButton: true
+    };
+  },
+  methods: {
+    onSignin() {
+      if (this.name) {
+        localStorage.setItem("nameUser", this.name)
+        this.$router.push({ name: "Home" });
+      }
+    },
+
+    nameInput() {
+      this.isActiveButton = this.name ? false : true
+    }
+  },
+};
+</script>
+
+<style scoped>
+.signin_sus {
+  position: absolute;
+  top: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.sigin_button {
+  margin-top: 20px;
+  width: 92%;
+}
+.sigin_text_input {
+  font-size: 1.37rem;
+  color: #fff;
+  margin-bottom: 20px;
+}
+.title_login {
+  padding-top: 150px;
+  font-size: 2.62rem;
+  color: #fff;
+  font-weight: 700;
+}
+.signin_logo-gov {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  height: 98%;
+}
+.el-row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.signin_left {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  background-color: rgba(59, 130, 246, 0.5);
+  height: 100vh;
+}
+
+.signin_right {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 100px;
+  background: radial-gradient(
+      389.07% 170.2% at 380.16% -72.26%,
+      rgba(255, 255, 255, 0.1) 21.58%,
+      rgba(255, 255, 255, 0) 100%
+    ),
+    radial-gradient(
+      285.83% 160.31% at 100% 13.66%,
+      rgba(202, 230, 252, 0) 0%,
+      rgba(202, 230, 252, 0) 51.74%,
+      rgba(202, 230, 252, 0.04) 51.76%
+    ),
+    radial-gradient(
+      208.91% 177.84% at 26.92% -25.46%,
+      rgba(202, 230, 252, 0) 0%,
+      rgba(202, 230, 252, 0) 60.7%,
+      rgba(202, 230, 252, 0.02) 60.76%
+    ),
+    linear-gradient(118.62deg, #003368 19.38%, #0069c5 129.43%);
+}
+
+.input-right {
+  margin-top: 10px;
+  max-width: 400px;
+}
+</style>
